5c10fe52c14726ada76e361f2631ae8c1824efe1
Thanakrit22 <yoyo221196@hotmail.com>
2020-11-06 12:36:53 +0100

    Added new number

---
+++ Assignments/Exercise 5.1.py
@@ -0,0 +1,13 @@
+a = ("Set the first number = 80")
+print(a)
+b = ("Set the second number = 25")
+print(b)
+c = ("80 + 25 = 105 ")
+print(c)
+d = ("80 - 25 = 55 ")
+print(d)
+e = ("80 * 25 = 2000 ")
+print(e)
+f = ("80 / 25 = 3.2 ")
+print(f)
+
