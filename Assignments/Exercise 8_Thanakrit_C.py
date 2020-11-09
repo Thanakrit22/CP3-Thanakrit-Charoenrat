a196e7580cc08ab064616c87e1fb77f7a603614a
Thanakrit22 <yoyo221196@hotmail.com>
2020-11-09 13:05:02 +0100

    exercise 8

---
+++ Assignments/Exercise 8_Thanakrit_C.py
@@ -0,0 +1,20 @@
+usernameInput = input("username :")
+passwordInput = input("Password :")
+if usernameInput == "Thanakrit22" and passwordInput == "yoyo2211":
+    print(" Your username and your password is correct")
+    print("--------Welcome to YoYo shop--------  ")
+    print("____________________________________")
+    print(" 1. Vat Calculator")
+    print(" 2. Price Calculator")
+    userSelected = int(input(">>"))
+    if userSelected == 1:
+        price = int(input("Price (KR)"))
+        Vat = 7
+        result = price + (price * 7 / 100)
+        print(result)
+    elif userSelected == 2:
+        price1 = int(input("First Product Price :"))
+        price2 = int(input("Second Product Price :"))
+        print(price1 + price2)
+else:
+    print (" Your username and your password is incorrect,please try again!!!!")
