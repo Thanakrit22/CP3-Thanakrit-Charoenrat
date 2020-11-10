532a05d3f7b199860cebf4175f2a0bb8982c10a1
Thanakrit22 <yoyo221196@hotmail.com>
2020-11-10 16:44:36 +0100

    Lecture50

---
+++ Assignments/Lecture 50_Thanakrit_C.py
@@ -0,0 +1,16 @@
+def addnumber(x,y):
+    print(x+y)
+    print("________________")
+addnumber(10,25)
+def Fornegative(k,t):
+    print(k-t)
+    print("__________________")
+Fornegative(90,50)
+def Formultiplication(b,a):
+    print(b*a)
+    print("___________________")
+Formultiplication(90,50)
+def Fordivision(c,j):
+    print(c/j)
+    print("___________________")
+Fordivision(90,50)
